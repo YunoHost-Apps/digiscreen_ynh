ce742a5a0547d00459d9fed861854176cc1de163
Félix Piédallu <felix.piedallu@non.se.com>
2023-10-03 14:03:10 +0200

    Fix nodejs

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,6 +16,7 @@ _digiscreen_build() {
         echo "VUE_APP_PIXABAY_API_KEY=$pixabay" >> .env
         echo "VUE_APP_GOOGLE_API_KEY=$googleYoutube" >> .env
 
+        ynh_use_nodejs
         ynh_npm install
         ynh_npm run build
     )
