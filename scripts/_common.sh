eef5c221b35dd0863ea39c35a1a513b707771126
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-10-03 13:11:15 +0200

    List PHP as dependency.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="php8.2-common"
 nodejs_version="14.19.3"
 
 #=================================================
