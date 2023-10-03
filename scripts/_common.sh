00038ad55cddd79127f20beef969609a8bc5ea23
Félix Piédallu <felix.piedallu@non.se.com>
2023-10-03 14:20:12 +0200

    Use major nodejs version , use pushd/popd instead of subshell

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,22 +4,21 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version="14.19.3"
+nodejs_version="14"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
 _digiscreen_build() {
-    (
-        cd "$install_dir/sources"
+    pushd "$install_dir/sources"
         echo "VUE_APP_PIXABAY_API_KEY=$pixabay" >> .env
         echo "VUE_APP_GOOGLE_API_KEY=$googleYoutube" >> .env
 
         ynh_use_nodejs
         ynh_npm install
         ynh_npm run build
-    )
+    popd
 }
 
 #=================================================
