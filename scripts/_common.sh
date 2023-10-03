d32907a76bb6ec51c0d3de356c55d49e2e563649
Félix Piédallu <felix.piedallu@non.se.com>
2023-10-03 16:06:31 +0200

    Change googleYoutube to apikey_google_youtube, pixabay to apikey_pixabay

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,8 +12,8 @@ nodejs_version="14"
 
 _digiscreen_build() {
     pushd "$install_dir/sources"
-        echo "VUE_APP_PIXABAY_API_KEY=$pixabay" >> .env
-        echo "VUE_APP_GOOGLE_API_KEY=$googleYoutube" >> .env
+        echo "VUE_APP_PIXABAY_API_KEY=$apikey_pixabay" >> .env
+        echo "VUE_APP_GOOGLE_API_KEY=$apikey_google_youtube" >> .env
 
         ynh_use_nodejs
         "$ynh_npm" install
