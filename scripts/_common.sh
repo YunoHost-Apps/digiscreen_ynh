8f24e8a95a01b60f2188ef5035efa2c32f345ec0
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-01-14 13:12:24 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,22 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version="14"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
-_digiscreen_build() {
-    pushd "$install_dir/sources"
-        echo "VUE_APP_PIXABAY_API_KEY=$apikey_pixabay" >> .env
-        echo "VUE_APP_GOOGLE_API_KEY=$apikey_google_youtube" >> .env
-
-        ynh_use_nodejs
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH $ynh_npm install
-        ynh_exec_warn_less sudo -u $app env $ynh_node_load_PATH $ynh_npm run build
-    popd
-}
 
 #=================================================
 # EXPERIMENTAL HELPERS
